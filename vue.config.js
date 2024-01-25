5bf9b5ac71f9d0778bc89ff45e3ee6ab51ed3ca5
zkwzkw428 <157690419+zkwzkw428@users.noreply.github.com>
2024-01-25 21:55:17 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  // 关闭eslint检查
+  lintOnSave: false,
+  // 配置代理跨域
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://gmall-h5-api.atguigu.cn",
+      }
+    }
+  }
+})
